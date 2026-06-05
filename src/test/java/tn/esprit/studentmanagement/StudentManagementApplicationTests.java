f86e9c26e84ee5cb14b1a04c8a0b0329ca61c762
Nidhal Gharbi <nidhalgharbi@gmail.com>
2026-06-05 20:29:53 +0100

    fix test context - exclude datasource

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,15 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
